758ef4b08118ff9f64018dc19756d4f9f401e7a0
Philipp Gadow <philippgadow@Philipps-MBP.fritz.box>
2021-07-13 09:58:18 +0200

    add ROC curve

--- bsm4tops-gnn/scripts/bsm4tops_nn.py
+++ bsm4tops-gnn/scripts/bsm4tops_nn.py
@@ -311,28 +311,24 @@ def doBDT(df):
 
 
     # ROC curve
-    from sklearn.metrics import roc_auc_score, roc_curve
-    for i in range(1):
-        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], twoclass_output_test[:, i])
-        roc_auc[i] = auc(fpr[i], tpr[i])
+    import sklearn.metrics as metrics
+    # calculate the fpr and tpr for all thresholds of the classification
+    probs = bdt.predict_proba(X_test)
+    preds = probs[:,1]
+    fpr, tpr, threshold = metrics.roc_curve(y_test, preds)
+    roc_auc = metrics.auc(fpr, tpr)
 
-    # Compute micro-average ROC curve and ROC area
-    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), twoclass_output_test.ravel())
-    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-
-    plt.figure()
-    lw = 2
-    plt.plot(fpr[2], tpr[2], color='darkorange',
-             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
-    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
+    # method I: plt
+    import matplotlib.pyplot as plt
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+    plt.legend(loc = 'lower right')
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
     plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic example')
-    plt.legend(loc="lower right")
+    plt.xlabel('False Positive Rate')
     plt.show()
-
 
 def main():
     # get histograms from files
